1dadb0828bba8b9b65f6b550744d3bf16e6f8e72
thanhloidev <chauthanhloi2k4@gmail.com>
2024-05-20 17:18:12 +0700

    backend

--- src/server.js
+++ src/server.js
@@ -3,10 +3,13 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine"
 import initWebRouters from './route/web'
 import connectDB from './config/connectDB';
+import cors from 'cors';
 require('dotenv').config();
 
 
 let app = express();
+app.use(cors({ credentials: true, origin: true }));
+
 
 // config app
 
@@ -18,8 +21,8 @@ viewEngine(app);
 initWebRouters(app);
 connectDB();
 
-let port = process.env.PORT || 8069;
-// port == undefine => port = 6969
+let port = process.env.PORT || 8080;
+// port == undefine => port = 8080
 app.listen(port,()=>{
     console.log("Backend Nodejs running  on port : "+ port)
 })
