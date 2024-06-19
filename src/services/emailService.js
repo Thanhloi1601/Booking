a2732a69507057cb281e43c33e71646703868f2c
thanhloidev <chauthanhloi2k4@gmail.com>
2024-06-19 10:18:59 +0700

    end

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -81,7 +81,7 @@ let sendAttackhment = async (dataSend) => {
       let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 587,
-        secure: false, // Use `true` for port 465, `false` for all other ports
+        secure: false, 
         auth: {
           user: process.env.EMAIL_APP,
           pass: process.env.EMAIL_APP_PASSWORD,
